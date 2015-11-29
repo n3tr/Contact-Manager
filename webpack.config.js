384b95bd9cbda1ced6b2dcfcdf11ec5ecbb3572a
Jirat K <saakyz@gmail.com>
2015-11-29 16:09:38 +0700

    Install dependencies and build tools
    - React, React-dom, jquery, backbone, underscore.
    - babel loader and present (react, es2015).
    - webpack and webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./app/js/app.js",
+  output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel?presets[]=react,presets[]=es2015'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': Object.keys(process.env).reduce(function(o, k) {
+        o[k] = JSON.stringify(process.env[k]);
+        return o;
+      }, {})
+    })
+  ]
+};
